65ff1afbd1e1944fe4d274ef0933e2ab213865cf
游泽旭 <3024768711@qq.com>
2026-06-23 13:11:56 +0800

    优化: CORS添加5176端口支持

--- home/user/projects/image-editor/image_editor/settings.py
+++ home/user/projects/image-editor/image_editor/settings.py
@@ -125,9 +125,11 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
+    'http://localhost:5176',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
     'http://127.0.0.1:5175',
+    'http://127.0.0.1:5176',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
